9fe318899abba0f542f3a03e3835f672f0d4c42a
Mashtixx <nquanghuy3456@gmail.com>
2024-03-18 18:06:12 -0400

    Fix 3

--- Executive Espionage/Assets/Scripts/PauseMenu.cs
+++ Executive Espionage/Assets/Scripts/PauseMenu.cs
@@ -7,6 +7,12 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GamePaused = false;
     public GameObject pauseMenu;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     public void Update()
     {
@@ -40,5 +46,37 @@ public class PauseMenu : MonoBehaviour
         Camera.main.gameObject.GetComponent<AudioSource>().volume *= 4;
         Time.timeScale = 1f;
         GamePaused = false;
+    }
+
+    public void SaveGame()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        PlayerPrefs.SetFloat("PlayerPosX", playerTransform.position.x);
+        PlayerPrefs.SetFloat("PlayerPosY", playerTransform.position.y);
+        PlayerPrefs.SetFloat("PlayerPosZ", playerTransform.position.z);
+        PlayerPrefs.SetFloat("PlayerRotX", playerTransform.rotation.eulerAngles.x);
+        PlayerPrefs.SetFloat("PlayerRotY", playerTransform.rotation.eulerAngles.y);
+        PlayerPrefs.SetFloat("PlayerRotZ", playerTransform.rotation.eulerAngles.z);
+
+        Debug.Log("Game saved.");
+    }
+
+    public void LoadGame()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        float posX = PlayerPrefs.GetFloat("PlayerPosX");
+        float posY = PlayerPrefs.GetFloat("PlayerPosY");
+        float posZ = PlayerPrefs.GetFloat("PlayerPosZ");
+        float rotX = PlayerPrefs.GetFloat("PlayerRotX");
+        float rotY = PlayerPrefs.GetFloat("PlayerRotY");
+        float rotZ = PlayerPrefs.GetFloat("PlayerRotZ");
+
+        Vector3 position = new Vector3(posX, posY, posZ);
+        Quaternion rotation = Quaternion.Euler(rotX, rotY, rotZ);
+
+        playerTransform.position = position;
+        playerTransform.rotation = rotation;
+
+        Debug.Log("Game loaded.");
     }
 }
